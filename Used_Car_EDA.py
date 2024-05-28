de98432670983a4a73b4efc31c03a31dc266b149
AmoghInfinity <156122521+AmoghInfinity@users.noreply.github.com>
2024-05-28 12:08:26 +0530

    Create Used_Car_EDA.py
    The main objective of this article is to cover the steps involved in Data pre-processing, Feature Engineering, and different stages of Exploratory Data Analysis, which is an essential step in any research analysis. 
    I've used a dataset containing information about about used cars and applied my knowledge of EDA to pre-process and analyze the data

---
+++ Used_Car_EDA.py
@@ -0,0 +1,80 @@
+import pandas as pd
+from datetime import date
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+year=date.today().year
+data=pd.read_csv(r"/content/used_cars_data.csv")
+data["Car_Age"]=year-data.Year
+data["Brand"]=data.Name.str.split().str.get(0)
+data["Model"]=data.Name.str.split().str.get(1)+data.Name.str.split().str.get(2)
+data=data.drop(['S.No.'], axis=1)
+data["Brand"].replace({'Land':'Land Rover','Mini':'Mini Cooper','ISUZU':'Isuzu'},inplace=True)
+cat_cols=data.select_dtypes(include=['object']).columns
+num_cols=data.select_dtypes(include=np.number).columns.tolist()
+for col in num_cols:
+  print(col)
+  print("skew:", round(data[col].skew(),2))
+  plt.figure(figsize=(12,9))
+  plt.subplot(1,2,1)
+  data[col].hist(grid=True)
+  plt.ylabel('Count')
+  plt.subplot(1,2,2)
+  sns.boxplot(x=data[col])
+  plt.show()
+fig, axes = plt.subplots(3, 2, figsize = (18, 18))
+fig.suptitle('Bar plot for all categorical variables in the dataset')
+sns.countplot(ax = axes[0, 0], x = 'Fuel_Type', data = data, color = 'blue',
+              order = data['Fuel_Type'].value_counts().index);
+sns.countplot(ax = axes[0, 1], x = 'Transmission', data = data, color = 'blue',
+              order = data['Transmission'].value_counts().index);
+sns.countplot(ax = axes[1, 0], x = 'Owner_Type', data = data, color = 'blue',
+              order = data['Owner_Type'].value_counts().index);
+sns.countplot(ax = axes[1, 1], x = 'Location', data = data, color = 'blue',
+              order = data['Location'].value_counts().index);
+sns.countplot(ax = axes[2, 0], x = 'Brand', data = data, color = 'blue',
+              order = data['Brand'].head(20).value_counts().index);
+sns.countplot(ax = axes[2, 1], x = 'Model', data = data, color = 'blue',
+              order = data['Model'].head(20).value_counts().index);
+axes[1][1].tick_params(labelrotation=45);
+axes[2][0].tick_params(labelrotation=90);
+axes[2][1].tick_params(labelrotation=90);
+def logt(data,col):
+  for colname in col:
+    if (data[colname]==1.0).all():
+      data[colname+'_log']=np.log(data[colname]+1)
+    else:
+      data[colname+'_log']=np.log(data[colname])
+logt(data,['Kilometers_Driven'])
+sns.displot(data["Kilometers_Driven_log"])
+logt(data,['Price'])
+sns.displot(data["Price_log"])
+data.info()
+plt.figure(figsize=(13,17))
+sns.pairplot(data=data.drop(['Kilometers_Driven','Price'],axis=1))
+plt.show()
+fig, axarr = plt.subplots(4, 2, figsize=(12, 18))
+data.groupby('Location')['Price_log'].mean().sort_values(ascending=False).plot.bar(ax=axarr[0][0], fontsize=12)
+axarr[0][0].set_title("Location Vs Price", fontsize=18)
+data.groupby('Transmission')['Price_log'].mean().sort_values(ascending=False).plot.bar(ax=axarr[0][1], fontsize=12)
+axarr[0][1].set_title("Transmission Vs Price", fontsize=18)
+data.groupby('Fuel_Type')['Price_log'].mean().sort_values(ascending=False).plot.bar(ax=axarr[1][0], fontsize=12)
+axarr[1][0].set_title("Fuel_Type Vs Price", fontsize=18)
+data.groupby('Owner_Type')['Price_log'].mean().sort_values(ascending=False).plot.bar(ax=axarr[1][1], fontsize=12)
+axarr[1][1].set_title("Owner_Type Vs Price", fontsize=18)
+data.groupby('Brand')['Price_log'].mean().sort_values(ascending=False).head(10).plot.bar(ax=axarr[2][0], fontsize=12)
+axarr[2][0].set_title("Brand Vs Price", fontsize=18)
+data.groupby('Model')['Price_log'].mean().sort_values(ascending=False).head(10).plot.bar(ax=axarr[2][1], fontsize=12)
+axarr[2][1].set_title("Model Vs Price", fontsize=18)
+data.groupby('Seats')['Price_log'].mean().sort_values(ascending=False).plot.bar(ax=axarr[3][0], fontsize=12)
+axarr[3][0].set_title("Seats Vs Price", fontsize=18)
+data.groupby('Car_Age')['Price_log'].mean().sort_values(ascending=False).plot.bar(ax=axarr[3][1], fontsize=12)
+axarr[3][1].set_title("Car_Age Vs Price", fontsize=18)
+plt.subplots_adjust(hspace=1.0)
+plt.subplots_adjust(wspace=.5)
+sns.despine()
+for col in num_cols:
+  plt.figure(figsize=(18,18))
+  sns.heatmap(data=data[col], annot=True, vmin=-1, vmax=1)
+  plt.show()
